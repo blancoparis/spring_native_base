166594099c2cb4219f2f2136a9f827c033b62668
blancoparis <blancoparis@gmail.com>
2024-03-21 21:47:35 +0100

    feat(test): configurar los test

--- src/test/java/com/example/demo/BaseApplicationTests.java
+++ src/test/java/com/example/demo/BaseApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("Unitarios")
 @SpringBootTest
 class BaseApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
---
+++ src/test/java/com/example/demo/integration/BaseIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.integration;
+
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
+
+@Tag("Integracion")
+@TestMethodOrder( MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseIntegrationTest {
+
+    @Test
+    public void test001HolaMundo(){
+        RestAssured.reset();
+        RestAssured.requestSpecification = new RequestSpecBuilder().
+                    setBaseUri("http://localhost")
+                    .setPort(8090)
+                    .setBasePath("")
+                    .build();
+        RestAssured.given()
+                .config(RestAssured.config())
+                    .log().all()
+                .when()
+                    .get("")
+                .then()
+                    .log().all()
+                    .assertThat().statusCode(200)
+                .body(
+                        Matchers.equalTo("hola mundo 2")
+                );
+    }
+}
