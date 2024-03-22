bbd596ec11c81b42370c725f1a1b56aa26c59744
blancoparis <blancoparis@gmail.com>
2024-03-22 21:41:12 +0100

    .

--- src/main/java/com/example/demo/BaseApplication.java
+++ src/main/java/com/example/demo/BaseApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class BaseApplication {
 
+	@Autowired
+	private Environment environment;
+
 	@RequestMapping("/")
 	String home(){
 		return "hola mundo 2";
 	}
 
+	public record Status(String status,String env){}
+	@RequestMapping("/status")
+	Status status(){
+		var entornos = String.join(",",environment.getActiveProfiles());
+		return new Status("OK",entornos);
+	}
 
 
 	public static void main(String[] args) {
